c2768e0a588ac53fe304332891a435eeaf09363e
prasad mhatre <prasadmhatre1297@gmail.com>
2022-10-14 19:46:15 +0530

    add jobitam div

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id' : 1,
+        'title' : 'Data Analyst',
+        'location' : 'Bengaluru, India',
+        'Salary' : 'Rs 10.00.000'
+    },
+    {
+        'id' : 2,
+        'title' : 'Data Scientist',
+        'location' : 'Delhi, India',
+        'Salary' : 'Rs 15.00.000'
+    },
+    {
+        'id' : 3,
+        'title' : 'Web Designer',
+        'location' : 'Remote',
+        
+    },
+    {
+        'id' : 4,
+        'title' : 'Backend Enginer',
+        'location' : 'Gujrat, India',
+        'Salary' : 'Rs 1.00.000'
+    },
+    
+]
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS,companyname='Prasad website')
+
+
+@app.route("/jobs")
+def list_jobs():   
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
